d2f71a1e82cb091081d939f843ca20561386cf2b
josh <stygian-phrygian@users.noreply.github.com>
2019-05-09 06:14:08 -0400

    wiring more of the UI to the engine, fixing bugs, i don't knw

--- script.js
+++ script.js
@@ -19,12 +19,18 @@ let midiOutput = {
         console.error("Midi is unavailable");
     }
 };
+// which channel to send events over
+let midiChannel = 0;
+// which velocity to send
+let midiVelocity = 96;
+// how much to transpose the events
+let transpose = 0;
 // our midi engine (initialized with dummy midi output)
 let engine = new Engine(midiOutput);
 // our selected keyboard layout
 let keyboardLayout;
 // which keys are *currently* held down and their requisite note values
-let pressedKeys2Note = {};
+let pressedKeys = {};
 ///////////////////////////////////////////////////////////////////////////////
 
 // after the window is loaded ---> initialize
@@ -37,6 +43,12 @@ function initialize() {
     }
 
     initializeKeyboardLayoutSelect(keyboardLayouts);
+    initializeArpeggiatorGateInput();
+    initializeDelayTimeInput();
+    initializeDelayRepeatInput();
+    initializeBPMInput();
+    initializeOctaveSelect();
+    initializeChannelSelect();
 
     // connect key presses/releases to midi note on/off events
     window.addEventListener("keydown", keyPressed);
@@ -45,11 +57,17 @@ function initialize() {
 }
 
 function keyPressed(e) {
-    console.log("pressed: ", e.key);
-    let pitch = keyboardLayout[e.key] + 48;
-    console.log("pitch: ", pitch);
-    engine.noteOn(pitch);
+    let pitch = keyboardLayout[e.key];
+    if (pitch !== undefined) {
+        // if we got a pitch from the layout
+        // create a new note and save it in the pressed keys
+        let note = new Note(pitch + transpose, midiVelocity, midiChannel);;
+        pressedKeys[e.key] = note;
+        // and trigger a note on in our engine
+        engine.noteOn(note.pitch, note.velocity, note.channel);
+    }
 
+    // other possible key behavior here
     switch (e.key) {
         case "Backspace":
             engine.releaseEverything();
@@ -61,8 +79,12 @@ function keyPressed(e) {
 }
 
 function keyReleased(e) {
-    let pitch = keyboardLayout[e.key] + 48;
-    engine.noteOff(pitch);
+    // try to fetch a corresponding note to release from the pressed keys
+    // ignore otherwise
+    let note = pressedKeys[e.key];
+    if (note !== undefined) {
+        engine.noteOff(note.pitch, note.velocity, note.channel);
+    }
 }
 
 
@@ -86,18 +108,18 @@ function midiAccessFailure() {
 
 // populate midi devices on UI, select a device, hook a change callback to our select element
 function initializeDeviceSelect(midiAccess) {
-    let deviceSelectElement = document.querySelector("#deviceSelect");
+    let selectElement = document.querySelector("#deviceSelect");
     // for each entry in available midi output ports
     for (var entry of midiAccess.outputs) {
         // append an option to our device select element
         let output = entry[1];
         let option = document.createElement("option");
         option.text = output.name;
-        deviceSelectElement.add(option);
+        selectElement.add(option);
     }
     // register a change callback for this device select element
-    deviceSelectElement.addEventListener("change", function(e) {
-        let selectedMidiOutputName = deviceSelectElement.value;
+    selectElement.addEventListener("change", (e) => {
+        let selectedMidiOutputName = e.target.value;
         for (let entry of midiAccess.outputs) {
             let output = entry[1];
             if (output.name == selectedMidiOutputName) {
@@ -108,24 +130,120 @@ function initializeDeviceSelect(midiAccess) {
         }
     });
     // pick a default midi output
-    deviceSelectElement.selectedIndex = "0";
-    deviceSelectElement.dispatchEvent(new Event("change"));
+    selectElement.selectedIndex = "0";
+    selectElement.dispatchEvent(new Event("change"));
 }
 
 // populate keyboard layouts on UI, select a layout, hook a change callback to our select element
 function initializeKeyboardLayoutSelect(keyboardLayouts) {
-    let keyboardLayoutSelectElement = document.querySelector("#keyboardLayoutSelect");
+    let selectElement = document.querySelector("#keyboardLayoutSelect");
     // create layout name options for our keyboard layout select element
     Object.keys(keyboardLayouts).forEach(keyboardLayoutName => {
         let option = document.createElement("option");
         option.text = keyboardLayoutName;
-        keyboardLayoutSelectElement.add(option);
+        selectElement.add(option);
     });
     // attach a callback for changes
-    keyboardLayoutSelectElement.addEventListener("change", () => {
-        keyboardLayout = keyboardLayouts[keyboardLayoutSelectElement.value];
+    selectElement.addEventListener("change", (e) => {
+        keyboardLayout = keyboardLayouts[e.target.value];
     });
     // pick a default option
-    keyboardLayoutSelectElement.selectedIndex = "0";
-    keyboardLayoutSelectElement.dispatchEvent(new Event("change"));
+    selectElement.selectedIndex = 0;
+    selectElement.dispatchEvent(new Event("change"));
+}
+
+function initializeOctaveSelect() {
+    let selectElement = document.querySelector("#octaveSection select");
+    // add a callback to the select element
+    selectElement.addEventListener("change", (e) => {
+        // we receive octave 4...
+        // c4 = midi 60 = 12*(4 + 1)
+        transpose = 12 * (parseInt(e.target.value) + 1);
+    });
+    // select a default option, let's pick whichever octave 3
+    let options = document.querySelectorAll("#octaveSection option");
+    let defaultOption;
+    options.forEach(option => {
+        if (parseInt(option.value) == 3) {
+            defaultOption = option;
+        }
+    });
+    selectElement.selectedIndex = defaultOption.index;
+    selectElement.dispatchEvent(new Event("change"));
+}
+
+function initializeChannelSelect() {
+    selectElement = document.querySelector("#channelSection select");
+    // add a callback to the select element
+    selectElement.addEventListener("change", (e) => {
+        midiChannel = parseInt(e.target.value);
+        console.log(midiChannel);
+    });
+    // select first
+    selectElement.selectedIndex = 0;
+    selectElement.dispatchEvent(new Event("change"));
+}
+
+function initializeBPMInput() {
+    let inputElement = document.querySelector("#bpmSection input");
+    // add callback to input element change
+    inputElement.addEventListener("change", (e) => {
+        let v = parseFloat(e.target.value);
+        // input[type=number] will APPARENTLY send invalid input outside of the
+        // min/max specified.  Our engine should handle it no problemo, the
+        // problemo is that the element will display an incorrect value there.
+        // We display whatever the engine allowed to occur.
+        engine.setBPM(v);
+        // update our UI
+        e.target.value = engine.BPM;
+    });
+    // fire initial event to set it
+    inputElement.dispatchEvent(new Event("change"));
+}
+
+function initializeArpeggiatorGateInput() {
+    let inputElement = document.querySelector("#arpeggiatorGateSection input");
+    let labelElement = document.querySelector("#arpeggiatorGateSection label");
+    // add callback to input element change
+    inputElement.addEventListener("change", (e) => {
+        let v = parseFloat(e.target.value);
+        // send value into engine
+        engine.setArpeggiatorGateTime(v);
+        // update our UI
+        labelElement.innerHTML = Math.trunc(v * 100).toString() + "%";
+    });
+    // fire initial event to set it
+    inputElement.dispatchEvent(new Event("change"));
+}
+
+function initializeDelayTimeInput() {
+    let inputElement = document.querySelector("#delayTimeSection input");
+    let labelElement = document.querySelector("#delayTimeSection label");
+    // add callback to input element change
+    inputElement.addEventListener("change", (e) => {
+        let v = parseInt(e.target.value);
+        // send value into engine
+        engine.setDelayTimeInMilliseconds(v);
+        // update our UI
+        labelElement.innerHTML = v.toString() + "ms";
+    });
+    // fire initial event to set it
+    inputElement.dispatchEvent(new Event("change"));
+}
+
+
+
+function initializeDelayRepeatInput() {
+    let inputElement = document.querySelector("#delayRepeatSection input");
+    let labelElement = document.querySelector("#delayRepeatSection label");
+    // add callback to input element change
+    inputElement.addEventListener("change", (e) => {
+        let v = parseInt(e.target.value);
+        // send value into engine
+        engine.setDelayRepeats(v);
+        // update our UI
+        labelElement.innerHTML = v.toString();
+    });
+    // fire initial event to set it
+    inputElement.dispatchEvent(new Event("change"));
 }
