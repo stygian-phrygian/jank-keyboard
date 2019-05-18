f7f929f1dd2d852e2bdcc2b254399a4f721b9471
josh <stygian-phrygian@users.noreply.github.com>
2019-05-18 19:32:03 -0400

    added janko variation and edited maj3 inverted to line up correctly with other scales

--- keyboardLayouts.js
+++ keyboardLayouts.js
@@ -80,6 +80,74 @@ const keyboardLayouts = {
         "=": 33,
     },
 
+    // whole tone scale layout (otherwise known as janko)
+    // but modified offset of columns
+    // columns ascending irregularly (z a q 1 => 0, 1, 12, 13) for chromatic notes
+    // rows ascending in major 2nds
+    //
+    // This has an irregular column ascension to account for the lack of chromatic
+    // coverage present in the whole tone scale.
+    "janko vers. 2": {
+        // col 1
+        "z": 0,
+        "a": -1,
+        "q": 10,
+        "1": 9,
+        // col 2
+        "x": 2,
+        "s": 1,
+        "w": 12,
+        "2": 11,
+        // col 3
+        "c": 4,
+        "d": 3,
+        "e": 14,
+        "3": 13,
+        // col 4
+        "v": 6,
+        "f": 5,
+        "r": 16,
+        "4": 15,
+        // col 5
+        "b": 8,
+        "g": 7,
+        "t": 18,
+        "5": 17,
+        // col 6
+        "n": 10,
+        "h": 9,
+        "y": 20,
+        "6": 19,
+        // col 7
+        "m": 12,
+        "j": 11,
+        "u": 22,
+        "7": 21,
+        // col 8
+        ",": 14,
+        "k": 13,
+        "i": 24,
+        "8": 23,
+        // col 9
+        ".": 16,
+        "l": 15,
+        "o": 26,
+        "9": 25,
+        // col 10
+        "/": 18,
+        ";": 17,
+        "p": 28,
+        "0": 27,
+        // col 11 (somewhat incomplete)
+        "'": 19,
+        "[": 30,
+        "-": 29,
+        // col 12 (somewhat incomplete)
+        "]": 32,
+        "=": 31,
+    },
+
+
 
     // melodic layout
     // columns ascending in perfect 4ths
@@ -221,62 +289,62 @@ const keyboardLayouts = {
     // no redundant notes (maximizing voicing options).
     "major 3rds inverted": {
         // col 1
-        "z": 3,
-        "a": 2,
-        "q": 1,
-        "1": 0,
+        "z": 0,
+        "a": -1,
+        "q": -2,
+        "1": -3,
         // col 2
-        "x": 7,
-        "s": 6,
-        "w": 5,
-        "2": 4,
+        "x": 4,
+        "s": 3,
+        "w": 2,
+        "2": 1,
         // col 3
-        "c": 11,
-        "d": 10,
-        "e": 9,
-        "3": 8,
+        "c": 8,
+        "d": 7,
+        "e": 6,
+        "3": 5,
         // col 4
-        "v": 15,
-        "f": 14,
-        "r": 13,
-        "4": 12,
+        "v": 12,
+        "f": 11,
+        "r": 10,
+        "4": 9,
         // col 5
-        "b": 19,
-        "g": 18,
-        "t": 17,
-        "5": 16,
+        "b": 16,
+        "g": 15,
+        "t": 14,
+        "5": 13,
         // col 6
-        "n": 23,
-        "h": 22,
-        "y": 21,
-        "6": 20,
+        "n": 20,
+        "h": 19,
+        "y": 18,
+        "6": 17,
         // col 7
-        "m": 27,
-        "j": 26,
-        "u": 25,
-        "7": 24,
+        "m": 24,
+        "j": 23,
+        "u": 22,
+        "7": 21,
         // col 8
-        ",": 31,
-        "k": 30,
-        "i": 29,
-        "8": 28,
+        ",": 28,
+        "k": 27,
+        "i": 26,
+        "8": 25,
         // col 9
-        ".": 35,
-        "l": 34,
-        "o": 33,
-        "9": 32,
+        ".": 32,
+        "l": 31,
+        "o": 30,
+        "9": 29,
         // col 10
-        "/": 39,
-        ";": 38,
-        "p": 37,
-        "0": 36,
+        "/": 36,
+        ";": 35,
+        "p": 34,
+        "0": 33,
         // col 11 (somewhat incomplete)
-        "'": 42,
-        "[": 41,
-        "-": 40,
+        "'": 39,
+        "[": 38,
+        "-": 37,
         // col 12 (somewhat incomplete)
-        "]": 45,
-        "=": 44,
+        "]": 42,
+        "=": 41,
     },
 
     // minor 3rds layout
